033ced3e3f0dceaae6150d22d35c77b15945371b
marina <marina@ultimatemovers.net>
2020-05-02 21:29:19 +0200

    add auth functionality and home design

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
+
+Route::get('/logout-manual', function (){
+    Auth::logout();
+    request()->session()->invalidate();
+});
 
 Route::get('/{any}', 'AppController@index')->where('any', '.*');
